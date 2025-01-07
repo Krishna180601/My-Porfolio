6cf5bf446b8ade1518a61509582d18996c7c0984
Krishna180601 <leela.ec.017@gmail.com>
2025-01-07 21:22:09 +0530

    Add footer section and move scripts to external file; enhance button styles

---
+++ script.js
@@ -0,0 +1,27 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+VANTA.DOTS({
+  el: "#header",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: 0xff004f,
+  color2: 0xff004f,
+  backgroundColor: 0x080808,
+  showLines: false,
+});
