77328a0cc662c160f22acadcf512d7ba1f32dc6e
Krishna180601 <leela.ec.017@gmail.com>
2025-02-21 13:24:43 +0530

    Remove unused images and enhance script functionality with Typed.js and ScrollReveal animations; update styles for improved layout and responsiveness

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// Typed.js
+var typed = new Typed(".auto-type", {
+  strings: [
+    "Appian Developer",
+    "Web Developer",
+    "UI/UX Designer",
+    "Freelancer",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+// Tab Functionality
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
@@ -11,6 +26,8 @@ function opentab(tabname) {
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabname).classList.add("active-tab");
 }
+
+// Landing Page Animation
 VANTA.DOTS({
   el: "#header",
   mouseControls: true,
@@ -25,3 +42,20 @@ VANTA.DOTS({
   backgroundColor: 0x080808,
   showLines: false,
 });
+
+// Scroll Reveal
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 80,
+});
+ScrollReveal().reveal(".header_text, .subtitle", { origin: "top" });
+ScrollReveal().reveal(
+  ".about-col-2, .col-1, .services-list, .work-list, .contact-right",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".contact-left,.about-col-1, nav img, .header_text h3", {
+  origin: "left",
+});
+ScrollReveal().reveal(".bio, nav ul, .quot, .gif", { origin: "right" });
